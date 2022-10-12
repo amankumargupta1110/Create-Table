f0cca6853b1d7c0cf345d5af4c8caa83d9f52e9f
AMAN KUMAR GUPTA <114344475+amankumargupta1110@users.noreply.github.com>
2022-10-12 22:39:13 +0530

    Add files via upload

---
+++ 10_pr_01.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number:\n"))
+for i in range(1, 11):
+    print(str(num) + " X " + str(i) + " = " + str(i*num))
+
+# 2nd Method "F-string"
+    # print(f"{num} X {i} = {num*1}")
